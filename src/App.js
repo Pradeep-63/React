db9830e52d6311add2cea32a8363ba40fb7a0066
pradeep-63 <pradeepjana2222@gmail.com>
2024-10-08 18:32:48 +0530

    push the hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useReducer, useState } from 'react';
 import './App.css';
+import MyComponent from './components/mycomponent';
+import { useEffect } from 'react';
+function reducer(state,action){
+  switch(action.type){
+     case 'incremented_age':{
+         return {age:state.age+1}
+     }
+     default:{
+      console.log("something went wrong");
+      
+     }
+  }
+}
 function App() {
+  const initialstate={age:42}
+  const[state,dispatch]=useReducer(reducer,initialstate)
+  function handleButtonClick() {
+    dispatch({ type: 'incremented_age' });
+  }
   return (
-   <div className='App'>
-    <h1>hello worlds</h1>
-   </div>
+    <>
+      <div className='state'>
+          <h1>{state.age}</h1>
+          <button onClick={handleButtonClick}>changeAge</button>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/mycomponent.js
@@ -0,0 +1,7 @@
+export default function MyComponent(proops) {
+    return (
+      <div>
+        <h1>{proops.message}</h1>
+      </div>
+    );
+  }
